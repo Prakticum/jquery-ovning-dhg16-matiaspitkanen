205ac9bb223217fcc6b4e8c7cd8f9b2c3f270e84
matiaspitkanen <42602525+matiaspitkanen@users.noreply.github.com>
2018-09-26 10:24:30 +0300

    Update jquery_ovn_1.js

--- jquery_ovn_1.js
+++ jquery_ovn_1.js
@@ -1,1 +1,41 @@
 
+// JavaScript Document
+var uppg4height = $("#uppg4-box").height()
+var uppg4width = $("#uppg4-box").width()
+
+console.log(uppg4height);
+console.log(uppg4width);
+
+$("#uppg4-box").html("H: " + uppg4height + " W: " + uppg4width);
+
+
+$(function(){
+	
+	$("p").addClass("uppg3class");
+	
+	var uppg4height = $("#uppg4-box").height();
+	
+	console.log(uppg4height);
+	
+	
+	$("#uppg5-btn").click(function(){
+		$("#uppg5-box").show("slow");
+		
+	});
+	
+	$("#uppg6-btn").click(function(){
+		$("#uppg6-box").addClass("uppg6-class");
+		
+	});
+	
+	$("#uppg7-btn").click(function(){
+		$("#uppg7-box").animate({
+			width: "+=100px",
+			height: "+=100px",
+			opacity: 0.5
+		}, 200);
+			
+	
+	});
+	
+});
